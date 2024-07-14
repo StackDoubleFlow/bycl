3c0e1164691ae688315114b881011bdaa5a0c7a5
StackDoubleFlow <ojaslandge@gmail.com>
2024-07-14 16:48:11 -0700

    Add a bunch of memory tests

--- asm_examples/build.sh
+++ asm_examples/build.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 BUILD_DIR=./build
 
 mkdir -p $BUILD_DIR
 
-clang --target=riscv32 -march=rv32i -Wl,-T../cc_fw/link.x -nostdlib $1 -o build/prog.elf
+clang --target=riscv32 -march=rv32i -Wl,-T../cc_fw/link.x -nostdlib -fuse-ld=lld $1 -o build/prog.elf
+
+# For debugging
+llvm-objdump -d build/prog.elf
+
 llvm-objcopy -O binary $BUILD_DIR/prog.elf $BUILD_DIR/prog.bin
 
-byclc $BUILD_DIR/prog.bin -o $BUILD_DIR/prog.schem
+../schem_gen/target/debug/byclc $BUILD_DIR/prog.bin -o $BUILD_DIR/prog.schem
