467f61e84f3d008c01eec13cc8fcaa567fadb4af
StackDoubleFlow <ojaslandge@gmail.com>
2024-12-15 21:18:38 -0600

    Add checkerboard demo

---
+++ tests/demos/checkerboard.c
@@ -0,0 +1,21 @@
+
+static inline void write(unsigned int data, unsigned int column) {
+  int volatile *data_ptr = (int *)0x80000000;
+  int volatile *col_ptr = (int *)0x80000004;
+  *data_ptr = data;
+  *col_ptr = column;
+}
+
+[[noreturn]] void entry() {
+  const unsigned int pattern = 0x55555555;
+  for (int col = 0; col < 32; col++) {
+    unsigned int data = pattern;
+    if ((col & 1) == 0) {
+      data <<= 1;
+    }
+    write(data, col);
+  }
+
+  while (1) {
+  }
+}
