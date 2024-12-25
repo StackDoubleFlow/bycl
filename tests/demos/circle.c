bf53e2a0e44d7ede8199dd7359bb473061056a43
StackDoubleFlow <ojaslandge@gmail.com>
2024-12-25 11:18:29 -0600

    tests: fix io ptrs in circle demo

--- tests/demos/circle.c
+++ tests/demos/circle.c
@@ -1,21 +1,21 @@
 
 static inline void write(unsigned int data, unsigned int column) {
-  int volatile *data_ptr = (int *) 0x8000000;
-  int volatile *col_ptr = (int *) 0x8000004;
+  int volatile *data_ptr = (int *) 0x80000000;
+  int volatile *col_ptr = (int *) 0x80000004;
   *data_ptr = data;
   *col_ptr = column;
 }
 
 void entry() {
-  const float radius = 16;
-  const float x_pos = 20;
-  const float y_pos = 15;
+  const int radius = 16;
+  const int x_pos = 15;
+  const int y_pos = 15;
 
   for (int col = 0; col < 32; col++) {
     unsigned int data = 0;
     for (int row = 0; row < 32; row++) {
-      float a = row - y_pos;
-      float b = col - x_pos;
+      int a = row - y_pos;
+      int b = col - x_pos;
       if (a * a + b * b < radius * radius) {
         data |= (1 << row);
       }
